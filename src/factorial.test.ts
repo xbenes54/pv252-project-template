2f6eac39790129a8ddbc491b7fcf8b60b24d7895
Marek Beneš <xbenes5@fi.muni.cz>
2024-09-27 16:32:35 +0200

    fix of fail test-unit

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -1,7 +1,7 @@
 import { factorial } from "./factorial.ts";
 
 test("factorial-5", () => {
-  expect(factorial(5)).toBe(121);
+  expect(factorial(5)).toBe(120);
 });
 
 test("factorial-minus", () => {
