13a18dc705f10910a7fbce88afe753a7135b4bb4
Marek Beneš <xbenes5@fi.muni.cz>
2024-09-27 17:01:50 +0200

    Add unit test to fibonacci negative

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,7 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonnaci-negative", () =>{
+  expect(() => fibonacci(-5)).toThrow("Cannot compute on negative numbers");
+});
