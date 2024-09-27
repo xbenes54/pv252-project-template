cb6c63fafb38b2c10a7a8cf93809ca32e6555938
Marek Beneš <xbenes5@fi.muni.cz>
2024-09-27 18:11:52 +0200

    Add test for redirect

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -5,3 +5,11 @@ test("find-watman", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
+
+test("redirect", async ({ page }) => {
+  await page.goto("/");
+
+  const flex = page.locator("#site-a");
+  await flex.click();
+  await expect(page).toHaveURL("/site_a.html");
+})
