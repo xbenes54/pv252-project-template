91903d88a8ffa763d3cfd9c30da863aa062bcadd
Marek Beneš <xbenes5@fi.muni.cz>
2024-09-28 13:27:45 +0200

    Add 4 tests for seznam.cz

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -13,3 +13,27 @@ test("redirect", async ({ page }) => {
   await flex.click();
   await expect(page).toHaveURL("/site_a.html");
 })
+
+test("content", async ({ page }) => {
+  await page.goto("https://www.seznam.cz", {timeout : 20000});
+  const logo = page.locator(".content");
+  await expect(logo).toBeVisible({ timeout: 30000 });
+});
+
+test("email-login-button", async ({ page }) => {
+  await page.goto("https://www.seznam.cz");
+  const loginButton = page.locator('#login');
+  await expect(loginButton).toBeInViewport({ timeout: 30000 });
+});
+
+test("article", async ({ page }) => {
+  await page.goto("https://www.seznam.cz");
+  const article = page.locator('.article').first();
+  await expect(article).toBeInViewport({ timeout: 30000 });
+});
+
+test("search", async ({ page }) => {
+  await page.goto("https://www.seznam.cz");
+  const search = page.locator('.search-form');
+  await expect(search).toBeInViewport({ timeout: 30000 });
+});
