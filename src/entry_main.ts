4a88bad42db6162e18c31143467c7f86cf84d1e3
Marek Beneš <xbenes5@fi.muni.cz>
2024-11-17 23:43:00 +0100

    Added state implementation

--- src/entry_main.ts
+++ src/entry_main.ts
@@ -1,9 +1,10 @@
-import { attr, css, FASTElement, html, observable, when, repeat, Observable, Subscriber } from "@microsoft/fast-element";
-import { reactive } from "@microsoft/fast-element/state.js";
+import {attr, css, FASTElement, html, observable, when, repeat, Observable, Subscriber} from "@microsoft/fast-element";
+import {reactive} from "@microsoft/fast-element/state.js";
 
-import { allComponents, provideFluentDesignSystem } from '@fluentui/web-components';
-import { loadFamousPeople, Person } from "./famous_people.js";
-import { Context } from "@microsoft/fast-element/context.js";
+import {allComponents, provideFluentDesignSystem} from '@fluentui/web-components';
+import {loadFamousPeople, Person} from "./famous_people.js";
+import {Context} from "@microsoft/fast-element/context.js";
+
 provideFluentDesignSystem().register(allComponents);
 
 // The probability of getting and error when loading a person.
@@ -14,197 +15,271 @@ const LOAD_TIME = 1000;
 const ITEM_COUNT = 20;
 
 class PersonListItem {
-  data: Person | null = null;
-  error: string | null = null;
+    data: Person | null = null;
+    error: string | null = null;
 
-  constructor(data: Person | null = null, error: string | null = null) {
-    this.data = data;
-    this.error = error;    
-  }
+    constructor(data: Person | null = null, error: string | null = null) {
+        this.data = data;
+        this.error = error;
+    }
 
-  /**
-   * True if the item is currently loading (i.e. it has no data).
-   */
-  isLoading(): boolean {
-    return this.data === null && this.error === null;
-  }
+    /**
+     * True if the item is currently loading (i.e. it has no data).
+     */
+    isLoading(): boolean {
+        return this.data === null && this.error === null;
+    }
 
-  /**
-   * True if the item was loaded with an error.
-   */
-  isError(): boolean {
-    return this.data === null && this.error !== null;
-  }
+    /**
+     * True if the item was loaded with an error.
+     */
+    isError(): boolean {
+        return this.data === null && this.error !== null;
+    }
 
-  /**
-   * True if the item is currently loaded properly and has data.
-   */
-  isOk(): boolean {
-    return this.data !== null && this.error === null;
-  }
+    /**
+     * True if the item is currently loaded properly and has data.
+     */
+    isOk(): boolean {
+        return this.data !== null && this.error === null;
+    }
 
-  /**
-   * Update the item with loaded person data.
-   */
-  setOk(data: Person) {
-    this.data = data;
-    this.error = null;
-  }
+    /**
+     * Update the item with loaded person data.
+     */
+    setOk(data: Person) {
+        this.data = data;
+        this.error = null;
+    }
 
-  /**
-   * Update the item with error string if loading failed.
-   */
-  setError(error: string) {
-    this.data = null;
-    this.error = error;
-  }
+    /**
+     * Update the item with error string if loading failed.
+     */
+    setError(error: string) {
+        this.data = null;
+        this.error = error;
+    }
 
-  /**
-   * Reset the data in this item to the default "loading" state.
-   */
-  setLoading() {
-    this.data = null;
-    this.error = null;
-  }
+    /**
+     * Reset the data in this item to the default "loading" state.
+     */
+    setLoading() {
+        this.data = null;
+        this.error = null;
+    }
 
 }
 
 export const PeopleListContext = Context.create<PeopleListContext>("Counter");
+
 export interface PeopleListContext {
-  // A list of "person-like" items. An item is either loading (no data available),
-  // or it loaded with an error (an error string is provided), or it is fully loaded.
-  people: PersonListItem[];
+    // A list of "person-like" items. An item is either loading (no data available),
+    // or it loaded with an error (an error string is provided), or it is fully loaded.
+    people: PersonListItem[];
 
-  // Indicates that at least one person in the list is still loading.
-  isLoading: boolean;  
+    // Indicates that at least one person in the list is still loading.
+    isLoading: boolean;
 
-  // The number of loaded elements.
-  loaded: number;
+    // The number of loaded elements.
+    loaded: number;
 
-  // Refresh element at a given position.
-  refresh(position: number): void;
+    // Refresh element at a given position.
+    refresh(position: number): void;
 }
 
 export class PeopleListContextElement extends FASTElement implements PeopleListContext {
 
-  @observable
-  people: PersonListItem[] = [];
+    @observable
+    people: PersonListItem[] = [];
 
-  @observable
-  isLoading: boolean = false;
+    @observable
+    isLoading: boolean = false;
 
-  @observable
-  loaded: number = 0;
+    @observable
+    loaded: number = 0;
 
-  // The full list of famous people that will serve as our "database". 
-  // This is just dummy data. Normally, you would fetch it from server or 
-  // retrieve it in some other way.
-  allPeople = loadFamousPeople();
+    // The full list of famous people that will serve as our "database".
+    // This is just dummy data. Normally, you would fetch it from server or
+    // retrieve it in some other way.
+    allPeople = loadFamousPeople();
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    // Initialize the list with initial empty items.
+        // Initialize the list with initial empty items.
 
-    // The "reactive" part is very important! It means that not only is the
-    // list itself observable, but also that the individual objects in that
-    // list will notify upon change any templates that use them.
-    let dummyData = [];
-    for (let i=0; i<ITEM_COUNT; i++) {
-      dummyData.push(reactive(new PersonListItem()));
-    }
-    this.people = dummyData;
-  }
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    // Provide this element as context to the child components.
-    PeopleListContext.provide(this, this);
-
-    // This code simulates a long running "loading" script that
-    // populates the "people" array one by one.
-    
-    this.isLoading = true;
-    this.loaded = 0;
-    const loadOne = () => {
-      if (Math.random() > ERROR_RATE) {
-        this.people[this.loaded].setOk(this.allPeople[this.loaded]);
-        console.log("Loaded", this.people[this.loaded]);        
-      } else {
-        this.people[this.loaded].setError("Failed to load.");
-        console.log("Failed to load.");
-      }
-      this.loaded += 1;
-      if (this.loaded < this.people.length) {
-        setTimeout(loadOne, LOAD_TIME);
-      } else {
-        this.isLoading = false;
-      }
-    }
-    setTimeout(loadOne, LOAD_TIME);
-  }
-
-  refresh(position: number) {
-    if (position >= 0 && position < this.people.length) {
-      // Mark the entry as loading.
-      if (this.people[position].isLoading()) {
-        return; // Already refreshing.
-      }
-      this.people[position].setLoading();
-      setTimeout(() => {
-        if (Math.random() > 0.2) {
-          this.people[position].setOk(this.allPeople[position]);
-          console.log("Reloaded", this.people[position]);        
-        } else {
-          this.people[position].setError("Failed to load after refresh.");
-          console.log("Failed to load.");
+        // The "reactive" part is very important! It means that not only is the
+        // list itself observable, but also that the individual objects in that
+        // list will notify upon change any templates that use them.
+        const dummyData = [];
+        for (let i = 0; i < ITEM_COUNT; i++) {
+            dummyData.push(reactive(new PersonListItem()));
         }
-      }, LOAD_TIME);
-    }    
-  }
+        this.people = dummyData;
+    }
+
+    connectedCallback(): void {
+        super.connectedCallback();
+        // Provide this element as context to the child components.
+        PeopleListContext.provide(this, this);
+
+        // This code simulates a long running "loading" script that
+        // populates the "people" array one by one.
+
+        this.isLoading = true;
+        this.loaded = 0;
+        const loadOne = () => {
+            if (Math.random() > ERROR_RATE) {
+                this.people[this.loaded].setOk(this.allPeople[this.loaded]);
+                console.log("Loaded", this.people[this.loaded]);
+            } else {
+                this.people[this.loaded].setError("Failed to load.");
+                console.log("Failed to load.");
+            }
+            this.loaded += 1;
+            if (this.loaded < this.people.length) {
+                setTimeout(loadOne, LOAD_TIME);
+            } else {
+                this.isLoading = false;
+            }
+        }
+        setTimeout(loadOne, LOAD_TIME);
+    }
+
+    refresh(position: number) {
+        if (position >= 0 && position < this.people.length) {
+            // Mark the entry as loading.
+            if (this.people[position].isLoading()) {
+                return; // Already refreshing.
+            }
+            this.people[position].setLoading();
+            setTimeout(() => {
+                if (Math.random() > 0.2) {
+                    this.people[position].setOk(this.allPeople[position]);
+                    console.log("Reloaded", this.people[position]);
+                } else {
+                    this.people[position].setError("Failed to load after refresh.");
+                    console.log("Failed to load.");
+                }
+            }, LOAD_TIME);
+        }
+    }
 
 }
 
 PeopleListContextElement.define({
-  name: "people-context",
-  template: html`<slot></slot>`,
-  styles: css``,
+    name: "people-context",
+    template: html`
+        <slot></slot>`,
+    styles: css``,
 })
 
 export class PersonElement extends FASTElement {
-  // Some suggestions for properties you might want to use:
+    @PeopleListContext context!: PeopleListContext;
+    @attr position: number = 0;
+    @observable person: PersonListItem = new PersonListItem();
 
-  // @PeopleListContext context!: PeopleListContext;
+    constructor(person: PersonListItem, position: number) {
+        super();
+        this.person = person;
+        this.position = position;
+    }
 
-  // @attr position: number = 0;
+    connectedCallback(): void {
+        super.connectedCallback();
+        if (this.person.isError()) {
+            setTimeout(() => {
+                this.context.refresh(this.position);
+            }, 1000);
+        }
+    }
 
-  // @observable person: PersonListItem = new PersonListItem();
+    handleRefresh() {
+        this.context.refresh(this.position);
+    }
 }
 
-const personElementTemplate = html<PersonElement>``;
-
+const personElementTemplate = html<PersonElement>`
+    ${when(x => x.person.isError(), html<PersonElement>`
+        <fluent-card style="padding: 16px; margin-bottom: 16px; height: 66px;">
+            <span style="display: inline-block; margin: 4px 16px 4px 16px;">Item failed to load.</span>
+            <fluent-button appearance="accent" style="float: left;" @click=${(x,) => x.handleRefresh()}>Refresh
+            </fluent-button>
+        </fluent-card>
+    `)}
+    ${when(x => x.person.isOk(), html<PersonElement>`
+        <fluent-card style="padding: 16px; margin-bottom: 16px;">
+            <fluent-breadcrumb>
+                <fluent-breadcrumb-item>${(x) => x.person.data?.continentName}</fluent-breadcrumb-item>
+                <fluent-breadcrumb-item>${(x) => x.person.data?.countryName}</fluent-breadcrumb-item>
+                <fluent-breadcrumb-item>${(x) => x.person.data?.birthcity}</fluent-breadcrumb-item>
+            </fluent-breadcrumb>
+            <h2 style="margin-top: 0px;">${(x) => x.person.data?.name}</h2>
+            <fluent-divider role="separator"></fluent-divider>
+            <p>This person was born in ${(x) => x.person.data?.birthyear} and is/was working as
+                ${(x) => x.person.data?.occupation} in the ${(x) => x.person.data?.industry} industry.</p>
+            <fluent-divider role="separator" style="margin-bottom: 16px;"></fluent-divider>
+            <a href="https://maps.google.com/?q=${(x) => x.person.data?.LAT},${(x) => x.person.data?.LON}"
+               target="_blank">
+                <fluent-button appearance="accent">Show on map</fluent-button>
+            </a>
+            <fluent-button appearance="outline" @click=${(x,) => x.handleRefresh()}>Refresh</fluent-button>
+        </fluent-card>
+    `)}
+    ${when(x => x.person.isLoading(), html<PersonElement>`
+        <fluent-skeleton style="height: 66px; padding: 16px; box-sizing: border-box;" shape="rect" shimmer="true">
+            Loading...
+        </fluent-skeleton>
+    `)}
+`;
 PersonElement.define({
-  name: "person-item",
-  template: personElementTemplate
+    name: "person-item",
+    template: personElementTemplate
 })
 
 export class PeopleList extends FASTElement {
-  // Probably will need to access the context state:
+    @PeopleListContext data!: PeopleListContext;
+    connectedCallback(): void {
+        super.connectedCallback();
+        this.renderPeople();
+        Observable.getNotifier(this.data).subscribe(<Subscriber>{
+            handleChange: (subject: PeopleListContext, args): void => {
+                if (args.propertyName === "loaded") {
+                    this.renderPeople();
+                }
+            }
+        });
+    }
 
-  // @PeopleListContext data!: PeopleListContext;
+    renderPeople(): void {
+        this.data.people.forEach((person, index) => {
+            setTimeout(() => {
+                const personElement = new PersonElement(person, index);
+                this.appendChild(personElement);
+            }, index * 1000);
+        });
 
-  connectedCallback(): void {
-    super.connectedCallback();
-
-    // This may be the place where you want to add child elements
-    // assuming they are not part of the template?
-  }
+    }
 }
 
 const personListTemplate = html<PeopleList>`
-<div></div>`
+    <div class="uk-width-1-1" style="padding: 16px;">
+        <div class="box">
+            <fluent-card style="padding: 16px; margin-bottom: 16px;">
+                <span style="display: block; margin-bottom: 8px;">Loaded ${(x) => x.data.loaded}/20:</span>
+                <fluent-progress max="20" value="${(x) => x.data.loaded}"></fluent-progress>
+            </fluent-card>
+            <slot></slot>
+            ${when(x => x.data.isLoading, html<PeopleList>`
+                <fluent-skeleton style="height: 66px; padding: 16px; box-sizing: border-box;" shape="rect"
+                                 shimmer="true">Loading...
+                </fluent-skeleton>
+            `)}
+        </div>
+    </div>\
+`
 
 PeopleList.define({
-  name: "people-list",
-  template: personListTemplate
+    name: "people-list",
+    template: personListTemplate
 })
